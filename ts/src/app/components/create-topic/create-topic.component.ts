af83b1375b3192abdc8a81755deff97c5d80864b
Thiagod3 <thiago.vilela3@outlook.com>
2023-10-09 13:33:38 -0300

    feat: create-topic-ts

--- ts/src/app/components/create-topic/create-topic.component.ts
+++ ts/src/app/components/create-topic/create-topic.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Status } from '../../enum/enum-status'
+
 @Component({
   selector: 'app-create-topic',
   templateUrl: './create-topic.component.html',
   styleUrls: ['./create-topic.component.scss']
 })
 export class CreateTopicComponent implements OnInit {
+  public Status = Status;
+  public status: Status = this.Status.Inicio;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  alterarStatus(novoStatus: Status) {
+    this.status = novoStatus;
   }
 
 }
---
+++ ts/src/app/enum/enum-status.ts
@@ -0,0 +1,5 @@
+export enum Status{
+    Inicio,
+    Enviar,
+    Criar
+}
