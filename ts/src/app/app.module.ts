56c09ecde39532f7ccd2bd0044338e4ba501ebb0
Thiagod3 <thiago.vilela3@outlook.com>
2023-10-09 12:51:03 -0300

    feat: setup-ts

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { VideoComponent } from './components/video/video.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { CreateTopicComponent } from './components/create-topic/create-topic.component';
+import { CommentsComponent } from './components/comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    HeaderComponent,
+    VideoComponent,
+    ResumeComponent,
+    CreateTopicComponent,
+    CommentsComponent,
   ],
   imports: [
     BrowserModule
